75b90d051d8c20b66d0256c7ede24f3889179853
Nathan Kimura <nathankimura14@gmail.com>
2022-02-03 15:27:13 -0300

    adiciona exercício 2 do dia 4.2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-jasvascript--array-e-loop-for/exercicios4-2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-jasvascript--array-e-loop-for/exercicios4-2.js
@@ -1,7 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //exercicio 1.
-
+/* 
 for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
+} */
+
+//exercicio 2.
+let resulado=0;
+for (let index = 0; index < numbers.length; index += 1) {
+    resulado+=numbers[index]
 }
+
+console.log(resulado);
