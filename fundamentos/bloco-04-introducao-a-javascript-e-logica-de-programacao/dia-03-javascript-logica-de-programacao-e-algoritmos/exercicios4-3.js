f88ab827eb76f06acf31f1c31169f399d3a5f3cc
Nathan Kimura <nathankimura14@gmail.com>
2022-02-04 18:53:02 -0300

    adiciona exercicio 2, dia 4.3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios4-3.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios4-3.js
@@ -1,8 +1,23 @@
-//exercicio 1
-
+//exercicio 1.
+/* 
 let fatorialDez=1;
 
 for (i=10;i>=1;i-=1){
     fatorialDez*=i;
 }
-console.log(fatorialDez);
+console.log(fatorialDez); 
+*/
+
+//exercicio 2.
+
+let word = 'banana';
+let wordArray= Array.from(word);
+let wordBackArray=[];
+//wordArray=[ 't', 'r', 'y', 'b', 'e', 'r' ]
+
+for (i=wordArray.length-1;i>=0;i-=1){
+    wordBackArray.push(wordArray[i])
+}
+let wordBack=wordBackArray.toString();
+console.log(wordBack);
+
