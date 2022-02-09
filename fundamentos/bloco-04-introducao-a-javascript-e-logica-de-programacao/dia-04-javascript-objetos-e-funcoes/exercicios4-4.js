850a2dc576bc7a17eaef6ad6b234ccd8d1b17e47
Nathan Kimura <nathankimura14@gmail.com>
2022-02-09 17:53:24 -0300

    atualiza pasta

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios4-4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios4-4.js
@@ -107,7 +107,7 @@ function retornaMaior(array){
 console.log(retornaMaior([2, 4, 6, 7, 10, 0, -3]));
  */
 //exercicio4.
-
+/* 
 function retornaMaiorNome(array){
     let maiorNome=[0];
     for (let index in array){
@@ -118,3 +118,51 @@ function retornaMaiorNome(array){
     return maiorNome;
 }
 console.log(retornaMaiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+ */
+
+//exercicio5.
+/* function maisRepetido(arrayNumeros){
+    let contaRepetidos=0;
+    let contaNumeros=0;
+    let indexNumeroRepetido=0;
+    for (let index in arrayNumeros){
+        verificaNumero = arrayNumeros[index];
+        for (let index2 in arrayNumeros){
+            if (verificaNumero===arrayNumeros[index2]){
+                contaNumeros+=1;
+            }
+        }
+        if (contaNumeros>contaRepetidos){
+            contaRepetidos=contaNumeros;
+            indexNumeroRepetido = index;
+        }
+        contaNumeros=0;
+    }
+    return arrayNumeros[indexNumeroRepetido];
+}
+console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3])); */
+
+//exercicio6.
+
+/* function somaNum(numeros) {
+    let soma = 0;
+    for (let index = 1; index <= numeros; index += 1) {
+      soma = soma + index;
+    }
+    return soma;
+  }
+  console.log(somaNum(5)); */
+
+  //exercicio7.
+function comparaFinal(word,ending){
+    wordSeparada = word.split('');
+    endingSeparada=ending.split('');
+    for(i=wordSeparada.length-1; i>=1; i-=1){
+        letraWord=wordSeparada[i];
+        for (i2=endingSeparada.length-1;i2>=1;i-=1){
+            if (endingSeparada[i2]===letraWord){
+
+            }
+        }
+    }
+}
