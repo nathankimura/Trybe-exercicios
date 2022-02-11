e03c1e7f720e45efe9bd638a6940705e23da150d
Nathan Kimura <nathankimura14@gmail.com>
2022-02-11 19:55:21 -0300

    adiciona exercício 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,36 +14,44 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
-//   1. puxar o local onde ficará a lista
-
-//   2. criar a lista
-
-//   3. customizar a lista
-function createDaysOfTheMonth (){
+//   1. puxar o local onde ficará a elemento
+//   2. criar a elemento
+//   3. customizar a elemento
+//   4. colocar a elemento no html
+//ex1
+function createDaysOfTheMonth() {
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-    const holidayList=[24, 25, 31];
-    const fridayList=[4, 11, 18, 25];
+    const holidayList = [24, 25, 31];
+    const fridayList = [4, 11, 18, 25];
     let daysPlace = document.querySelector('#days');
-    for (index=0; index<dezDaysList.length;index+=1){
+    for (index = 0; index < dezDaysList.length; index += 1) {
         let dayListLi = document.createElement('li');
-        let daysCurrent=dezDaysList[index];
+        let daysCurrent = dezDaysList[index];
         dayListLi.innerHTML = daysCurrent;
         dayListLi.className = 'day';
 
         daysPlace.appendChild(dayListLi);
 
-        for (index2=0; index2<holidayList.length; index2+=1){
-            if (daysCurrent===holidayList[index2]){
-                dayListLi.classList.add('holiday'); 
+        for (index2 = 0; index2 < holidayList.length; index2 += 1) {
+            if (daysCurrent === holidayList[index2]) {
+                dayListLi.classList.add('holiday');
             }
         }
-        for (index3=0; index3<fridayList.length; index3+=1){
-            if (daysCurrent===fridayList[index3]){
-                dayListLi.classList.add('friday'); 
+        for (index3 = 0; index3 < fridayList.length; index3 += 1) {
+            if (daysCurrent === fridayList[index3]) {
+                dayListLi.classList.add('friday');
             }
         }
     }
 }
 createDaysOfTheMonth();
 
-//   4. colocar a lista no html
+//ex2.
+function createBtn(holiday) {
+    let btnHoliday = document.querySelector('.buttons-container');
+    let btn = document.createElement('button');
+    btn.innerHTML = holiday;
+    btn.id = 'btn-'+holiday;
+    btnHoliday.appendChild(btn);  
+}
+createBtn('Feriados')
