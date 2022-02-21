474e50addb3c458ab5c1544e945538bd96bd11b2
Nathan Kimura <nathankimura14@gmail.com>
2022-02-21 15:31:14 -0300

    adiciona arquivos

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,24 @@
+function prevent(event){
+event.preventDefault();
+}
+window.onload = function(){
+    let button = document.querySelector("#buttonSubmit")
+button.addEventListener('click',prevent);}
+
+function clearInfo(){
+    let infos = document.querySelectorAll('input');
+    let textArea = document.querySelector('textarea');
+    for (let index=0; index<infos.length;index+=1){
+        let inputs = infos[index];
+        inputs.value="";
+        inputs.checked = false;
+    }
+    textArea.value="";
+
+}
+window.onload = function () {
+    let button = document.querySelector('#buttonSubmit');
+    button.addEventListener('click', prevent);
+    let clearButton = document.querySelector('#clearButton');
+    clearButton.addEventListener('click', clearInfo);
+  };
