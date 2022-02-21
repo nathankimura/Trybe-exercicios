9a6577fc4bddc726c593850ec97e7225941f24b0
Nathan Kimura <nathankimura14@gmail.com>
2022-02-21 18:47:06 -0300

    adiciona o que foi feito até o momento

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,24 +1,33 @@
-function prevent(event){
-event.preventDefault();
-}
-window.onload = function(){
-    let button = document.querySelector("#buttonSubmit")
-button.addEventListener('click',prevent);}
+// function prevent(event){
+// event.preventDefault();
+// }
+// window.onload = function(){
+//     let button = document.querySelector("#buttonSubmit")
+// button.addEventListener('click',prevent);}
 
-function clearInfo(){
-    let infos = document.querySelectorAll('input');
-    let textArea = document.querySelector('textarea');
-    for (let index=0; index<infos.length;index+=1){
-        let inputs = infos[index];
-        inputs.value="";
-        inputs.checked = false;
-    }
-    textArea.value="";
+// function clearInfo(){
+//     let infos = document.querySelectorAll('input');
+//     let textArea = document.querySelector('textarea');
+//     for (let index=0; index<infos.length;index+=1){
+//         let inputs = infos[index];
+//         inputs.value="";
+//         inputs.checked = false;
+//     }
+//     textArea.value="";
 
-}
-window.onload = function () {
-    let button = document.querySelector('#buttonSubmit');
-    button.addEventListener('click', prevent);
-    let clearButton = document.querySelector('#clearButton');
-    clearButton.addEventListener('click', clearInfo);
-  };
+// }
+// window.onload = function () {
+//     let button = document.querySelector('#buttonSubmit');
+//     button.addEventListener('click', prevent);
+//     let clearButton = document.querySelector('#clearButton');
+//     clearButton.addEventListener('click', clearInfo);
+//   };
+window.addEventListener('DOMContentLoaded', function(){
+    let myDatepicker = document.querySelector('input[name="demo"]');
+    myDatepicker.DatePickerX.init({
+        mondayFirst      : true,
+        format           : 'yyyy/mm/dd',
+        todayButtonLabel : 'Hoje',
+        clearButtonLabel : 'Limpar'
+    });
+  });
