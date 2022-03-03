b32621d3922a1932103f71429c1b880b1510a35c
Nathan Kimura <nathankimura14@gmail.com>
2022-03-03 16:10:22 -0300

    adiciona exercício 2 do dia 7.1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,11 +1,21 @@
-const testingScope = (escopo) => {
-    if (escopo === true) {
-        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-        ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-        console.log(ifScope);
-    } else {
-        const elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-        console.log(elseScope);
-    }
+// const testingScope = (escopo) => {
+//     if (escopo === true) {
+//         let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//         ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//         console.log(ifScope);
+//     } else {
+//         const elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+//         console.log(elseScope);
+//     }
+// }
+// testingScope(true);
+
+// exercício 2.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const OrdenaOddsAndEvans = () =>{
+    oddsAndEvens.sort((a, b) => a - b);
+    return oddsAndEvens;
 }
-testingScope(true);
+const sortedArray = OrdenaOddsAndEvans();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
